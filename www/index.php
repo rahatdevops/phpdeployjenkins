7537e4371e05e95f617bd9c660197568e6ffd467
rahatdevops <118961502+rahatdevops@users.noreply.github.com>
2023-08-28 14:23:14 +0600

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
  <head>
-  <title>Ranjit Swain | Docker class</title>
+  <title>Saeed Mahmud Rahat | Docker class</title>
 
   <meta charset="utf-8">
 
@@ -11,11 +11,11 @@
 </head>
 <body>
     <div class="container">
-    <?php echo "<h1>Hi! I'm Ranjit</h1>"; ?>
+    <?php echo "<h1>Hi! I'm Rahat</h1>"; ?>
 	<?php echo "<h3>This is Docker Compose Class!</h3>"; ?>
 
     <?php
-    $conn = mysqli_connect('db', 'ranjit', 'ranjit', "myDb");
+    $conn = mysqli_connect('db', 'rahat', 'rahat', "myDb");
     $query = 'SELECT * From Person';
     $result = mysqli_query($conn, $query);
     echo '<table class="table table-striped">';
